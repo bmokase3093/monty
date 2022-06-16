43deffb2b604e02cb142c093991f05c8fe8ae267
bmokase3093 <bmokase3093@gmail.com>
2022-06-16 11:07:41 -0700

    Test monty swap add pull

--- monty.h
+++ monty.h
@@ -1,16 +1,20 @@
 #ifndef MONTY_H
 #define MONTY_H
-#include <fcntl.h>
-#include <unistd.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/*--- Struct Definitions ---*/
+extern int push_arg;
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -22,10 +26,9 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
@@ -36,40 +39,51 @@ typedef struct instruction_s
 } instruction_t;
 
 /**
- * struct globalvariables - global struct of variables
- * @num: value of node
- * @line: line from getline
- * @mfile: textfile
- * @stack: pointer to the head of stack_t
- * @filename: file name string
- * @token: token
- *
- * Description: variables to share between functions
+ * struct global_variable - opcoode and its function
+ * @file: the opcode
+ * @push_arg: function to handle the opcode
+ * @buffer: pointer to
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct globalvariables
+typedef struct global_variable
 {
-	int num;
-	char *line;
-	FILE *mfile;
-	stack_t *stack;
-	char *filename;
-	char *token;
-} gbv;
+	FILE *file;
+	int push_arg;
+	char *buffer;
+} global_var;
 
-extern gbv gv;
+extern global_var var_global;
 
-void errorHandler(unsigned int errno, unsigned int line_number);
-void errorHandler2(unsigned int errno, unsigned int line_number);
-char *isNumber(char *value, unsigned int line_number);
-void compare(char *token, stack_t **stack, unsigned int line_number);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
+void read_file(char *filename, stack_t **stack);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+instruct_func get_op_func(char *str);
 
-void freeAll(void);
-void freeLinkedList(stack_t *stack)
+/*Fuxntions Monty*/
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+/*Functions string ascci */
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+
+/*Functions math*/
+void _sub(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+
+/*Utility functions*/
+void free_dlistint(stack_t *head);
+void dobfree(stack_t **stack);
+void dobfree2(stack_t **stack);
+int _isalpha(int c);
 #endif
